3f6ac6eac99a328d04a4a607dffaf41fe90b5ab0
Akanksh Pratheek <akanksh.pratheek@gmail.com>
2018-03-03 13:09:53 +0530

    video snapshot

--- cv_1_0.py
+++ cv_1_0.py
@@ -1,9 +1,26 @@
 import numpy as np
 import cv2
-img1 = cv2.imread('bnp2.jpeg',0)
+'''
+cap = cv2.VideoCapture('ball_test.h264')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imwrite('camframe2.jpeg',gray)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+'''
+img1 = cv2.imread('camframe2.jpeg')
 cv2.imshow('gray',img1)
 
-retval, img2	=	cv2.threshold(img1,100, 100, cv2.THRESH_BINARY_INV)
+retval, img2	=	cv2.threshold(img1,80, 100, cv2.THRESH_BINARY_INV)
 cv2.imshow('threshold', img2)
 
 colsum = np.sum(img2,axis = 0)
